c2378742ddf56e9406544ce0b8d2e157e9fbe849
Vlad Emelianov <volshebnyi@gmail.com>
2023-03-29 02:19:18 +0300

    Sync helpers

--- .github/workflows/helpers.js
+++ .github/workflows/helpers.js
@@ -1,4 +1,19 @@
-const https = require('https')
+const assert = require('assert')
+
+let fetch = null;
+let core = null;
+let context = null;
+
+function setupGlobals(globals) {
+    fetch = globals.fetch
+    assert(fetch, 'fetch is not defined')
+
+    core = globals.core
+    assert(core, 'core is not defined')
+
+    context = globals.context
+    assert(context, 'context is not defined')
+}
 
 function sortVersions(versions) {
     return versions.map(
@@ -9,28 +24,9 @@ function sortVersions(versions) {
 }
 
 async function getReleaseVersions(packageName) {
-    const options = {
-        hostname: 'pypi.org',
-        port: 443,
-        path: `/pypi/${packageName}/json`,
-        method: 'GET'
-    }
-
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, res => {
-            if (res.statusCode < 200 || res.statusCode >= 300) {
-                return reject(new Error(`Status Code: ${res.statusCode}`))
-            }
-            const data = []
-            res.on('data', chunk => data.push(chunk))
-            res.on('end', () => {
-                const response = JSON.parse(Buffer.concat(data).toString())
-                resolve(Object.keys(response.releases))
-            })
-        })
-        req.on('error', reject)
-        req.end()
-    })
+    const response = await fetch(`https://pypi.org/pypi/${packageName}/json`)
+    const data = await response.json()
+    return Object.keys(data.releases)
 }
 
 async function getLatestVersion(package) {
@@ -65,11 +61,40 @@ function getNextVersion(version) {
     return `${stableVersion}.post${parseInt(post, 10) + 1}`
 }
 
+async function extractVersions() {
+    core.setOutput('stubs-version', '')
+    const package = process.env.PACKAGE
+    const stubsPackage = process.env.STUBS
+    const force = context.payload.inputs ? context.payload.inputs.force !== 'false' : false
+
+    if (force) {
+        core.notice('Force release, skipping version check')
+    }
+
+    const inputVersion = context.payload.inputs?.version
+    const version = inputVersion ? inputVersion : await getLatestVersion(package)
+    core.notice(`${package} version = ${version}`)
+
+    const stubsVersion = await getLatestStubsVersion(stubsPackage, version)
+    core.notice(`${stubsPackage} latest version = ${stubsVersion}`)
+
+    const buildStubsVersion = stubsVersion ? getNextVersion(stubsVersion) : version
+
+    if (!force) {
+        const isStubsVersionGreater = stubsVersion === null || isStableVersionGreater(buildStubsVersion, stubsVersion)
+        if (!isStubsVersionGreater) {
+            core.notice('Stubs version is not greater than the latest, skipping run')
+            return
+        }
+    }
+
+    core.notice(`New ${package} version found: ${version}`)
+    core.notice(`${stubsPackage} build version = ${buildStubsVersion}`)
+    core.setOutput('version', version)
+    core.setOutput('stubs-version', buildStubsVersion)
+}
+
 module.exports = {
-    getLatestVersion,
-    isVersionGreater,
-    getStableVersion,
-    isStableVersionGreater,
-    getLatestStubsVersion,
-    getNextVersion
+    setupGlobals,
+    extractVersions
 }

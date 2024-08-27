c1f26aeedc31887ea312cb222f9cf46d64bfc17c
Vlad Emelianov <volshebnyi@gmail.com>
2024-08-27 05:50:52 +0300

    Use uv instead of poetry, bump to py38

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,8 +4,5 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-poetry run pyright
-poetry run ruff check
-poetry run ruff format --check
-poetry run mypy awscrt-stubs
-poetry run istub -u
+uv run pre-commit run --all-files
+uv run istub -u

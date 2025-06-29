bf5b5c1b9db74bdf77b5c638a5e8869c531ef3f1
Vlad Emelianov <volshebnyi@gmail.com>
2025-06-30 01:45:49 +0300

    Update stubs to 0.27.4

--- awscrt-stubs/__init__.pyi
+++ awscrt-stubs/__init__.pyi
@@ -9,6 +9,8 @@ from awscrt import crypto as crypto
 from awscrt import http as http
 from awscrt import io as io
 from awscrt import mqtt as mqtt
+from awscrt import mqtt5 as mqtt5
+from awscrt import mqtt_request_response as mqtt_request_response
 from awscrt import s3 as s3
 from awscrt import websocket as websocket
 
@@ -18,6 +20,8 @@ __all__ = [
     "http",
     "io",
     "mqtt",
+    "mqtt5",
+    "mqtt_request_response",
     "s3",
     "websocket",
 ]

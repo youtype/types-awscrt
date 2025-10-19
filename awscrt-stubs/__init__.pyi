0b7636dbdfcf7e68b76c043685b753566dba8025
Vlad Emelianov <volshebnyi@gmail.com>
2025-10-19 13:33:01 +0700

    Update stubs

--- awscrt-stubs/__init__.pyi
+++ awscrt-stubs/__init__.pyi
@@ -4,6 +4,7 @@ Type annotations for awscrt module.
 Copyright 2024 Vlad Emelianov
 """
 
+from awscrt import aio as aio
 from awscrt import auth as auth
 from awscrt import crypto as crypto
 from awscrt import http as http
@@ -15,6 +16,7 @@ from awscrt import s3 as s3
 from awscrt import websocket as websocket
 
 __all__ = [
+    "aio",
     "auth",
     "crypto",
     "http",
--- awscrt-stubs/aio/__init__.pyi
+++ awscrt-stubs/aio/__init__.pyi
@@ -3,3 +3,7 @@ Type annotations for awscrt.aio module.
 
 Copyright 2025 Vlad Emelianov
 """
+
+from awscrt.aio import http
+
+__all__ = ["http"]
